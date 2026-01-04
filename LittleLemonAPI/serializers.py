d29468a52503c032fd59a57fe9b9169af9f8826f
Abdulrahman Sabry <abdulrahman.sabry@allendevaux.com>
2026-01-05 01:26:29 +0200

    Add MenuItemSerializer with validation and tax calculation methods

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -1,7 +1,24 @@
+from decimal import Decimal
 from rest_framework import serializers
 from .models import MenuItem
 
 class MenuItemSerializer(serializers.ModelSerializer):
+    stock = serializers.IntegerField(source='inventory')
+    category = serializers.StringRelatedField()
+    tax = serializers.SerializerMethodField(method_name='calculate_tax')
     class Meta:
         model = MenuItem
-        fields = ['id', 'title', 'price', 'inventory']
+        fields = ['id', 'title', 'price', 'stock', 'category', 'tax']
+    
+    def validate_price(self, value):
+        if value < 0:
+            raise serializers.ValidationError("Price must be a positive number.")
+        return value
+    
+    def validate_stock(self, value):
+        if value < 0:
+            raise serializers.ValidationError("Stock must be a non-negative integer.")
+        return value
+    
+    def calculate_tax(self, product: MenuItem):
+        return product.price * Decimal('0.1')
