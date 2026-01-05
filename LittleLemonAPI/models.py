dc084173bfb610be64e647fadefdb1ec514266db
Abdulrahman Sabry <abdulrahman.sabry@allendevaux.com>
2026-01-06 00:59:21 +0200

    Add Category model and update MenuItem to include category field with related serializer methods

--- LittleLemonAPI/models.py
+++ LittleLemonAPI/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Category(models.Model):
+    id = models.IntegerField(primary_key=True, auto_created=True)
     slug = models.SlugField()
     title = models.CharField(max_length=255)
 
@@ -8,3 +9,4 @@ class MenuItem(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.SmallIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='menu_items', default=1)
--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -9,11 +9,12 @@ class CategorySerializer (serializers.ModelSerializer):
 
 class MenuItemSerializer(serializers.ModelSerializer):
     stock = serializers.IntegerField(source='inventory')
-    category = serializers.StringRelatedField()
     price_after_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    # category = CategorySerializer()
     class Meta:
         model = MenuItem
         fields = ['id', 'title', 'price', 'stock', 'category', 'price_after_tax']
+        depth = 1
     
     def validate_price(self, value):
         if value < 0:
@@ -24,6 +25,12 @@ class MenuItemSerializer(serializers.ModelSerializer):
         if value < 0:
             raise serializers.ValidationError("Stock must be a non-negative integer.")
         return value
+    
+    def create(self, validated_data):
+        category_data = validated_data.pop('category')
+        category, created = Category.objects.get_or_create(**category_data)
+        menu_item = MenuItem.objects.create(category=category, **validated_data)
+        return menu_item
     
     def calculate_tax(self, product: MenuItem):
         return product.price * Decimal('0.1')
