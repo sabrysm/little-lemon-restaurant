0b49b89393e880cc1f1452c245510f4710c00abc
Abdulrahman Sabry <abdulrahman.sabry@allendevaux.com>
2026-01-07 01:21:45 +0200

    Remove unnecessary id field from Category model and update MenuItem category relation

--- LittleLemonAPI/models.py
+++ LittleLemonAPI/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Category(models.Model):
-    id = models.IntegerField(primary_key=True, auto_created=True)
     slug = models.SlugField()
     title = models.CharField(max_length=255)
 
@@ -9,4 +8,4 @@ class MenuItem(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.SmallIntegerField()
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='menu_items', default=1)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
